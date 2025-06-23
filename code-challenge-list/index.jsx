111efc2f1bff67a8a4195e28e9bd5eef3c9b903c
nevets-steven <nevets322@gmail.com>
2025-06-23 16:19:13 -0400

    added challenge

---
+++ code-challenge-list/index.jsx
@@ -0,0 +1,21 @@
+//  * Challenge: Set up a React app from scratch again.
+//  * This time, try rendering an unordered list with 2-3 list items inside
+//  * with why you're excited to be learning React.
+//  
+
+import { createRoot } from "react-dom/client";
+
+const root = ReactDOM(createRoot(document.getElementById('root')));
+const renderHTML = 
+
+`
+<h1>Why I Am Excited To Learn React</h1>
+<div>
+    <ul>
+        <li>I love learning new languages, and testing my limits.</li>
+        <li>A lot of jobs need or require React knowlexge</li>
+        <li>Enhancing my learning of JS, HTML and CSS</li>
+    </ul>
+</div>
+`
+root.render(renderHTML);
