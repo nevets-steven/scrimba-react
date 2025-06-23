5a5b2ac4e339924bdbadf17a4aa09eb562aaa6df
nevets-steven <nevets322@gmail.com>
2025-06-23 16:09:28 -0400

    finsihed lesson 1

---
+++ first-react-code/index.jsx
@@ -0,0 +1,8 @@
+// 1. create a root
+
+// 2. render some markup to the root
+
+// script in html are importing packagess
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<h1>Hello from React!</h1>);
