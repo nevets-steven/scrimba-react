5dbcd04915bf94f5484ae4fbd3c81d4b14d019ae
nevets-steven <nevets322@gmail.com>
2025-06-24 11:44:42 -0400

    added jsx folders one with challenge

---
+++ jsx/index.jsx
@@ -0,0 +1,9 @@
+
+// import { createRoot } from 'react-dom/client'
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+const reactElement = <h1>Hello from JSX</h1>
+
+console.log(reactElement)
+
+root.render(reactElement)
