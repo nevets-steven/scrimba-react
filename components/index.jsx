63007f3ce64d4d4e4230efb67ef5a697d3e02a67
nevets-steven <nevets322@gmail.com>
2025-06-30 16:24:27 -0400

    udpated components.jsx to challenge 2

--- components/index.jsx
+++ components/index.jsx
@@ -42,13 +42,36 @@ function TemporaryName() {
 
 function Page() {
     return (
-        <section id="ordered-list">
-            <ol className="list">
-                <li className="list-item">I love learning</li>
-                <li className="list-item">React is in demand</li>
-                <li className="list-item">I find this fun</li>
-            </ol>
-        </section>
+        <main>
+            <header id="header">
+                <img src="react-logo.png" alt="React Logo" width='40px'/>
+                <h1 className="heading">Reasons I'm excited to learn React</h1>
+            </header>
+            <section id="ordered-list">
+                <ol className="list">
+                    <li className="list-item">I love learning</li>
+                    <li className="list-item">React is in demand</li>
+                    <li className="list-item">I find this fun</li>
+                </ol>
+            </section>
+        </main>
+        
     )
 }
+
+/**
+Challenge: 
+
+Part 2: 
+- Add a `<header>` element with an `<img />` element with the image of the 
+  React logo inside (src="react-logo.png") and make sure to set the 
+  width to something more manageable so it doesn't take up the whole screen.
+  Also, as always, you should include some alt text on the image.
+- Add an `<h1>` with some text describing the page. (E.g. "Reasons
+  I'm excited to learn React"). Place it above the ordered list, then wrap
+  the `<h1>` and `<ol>` inside a `<main>` element to keep our semantic
+  structure flowing well.
+- Add a `<footer>` after the list that says: 
+    "© 20xx <last name here> development. All rights reserved."
+ */
 
