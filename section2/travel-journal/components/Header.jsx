3a480c9c83a7e61ea947c44a409d00ddda714bf1
nevets-steven <nevets322@gmail.com>
2025-07-08 20:31:27 -0400

    restructured files and began data driven react

---
+++ section2/travel-journal/components/Header.jsx
@@ -0,0 +1,12 @@
+// export default funciton Header(){
+//     return()
+// }
+
+function Header(){
+    return(
+        <header id="header">
+            <img src="./Globe.png" alt="Globe" />
+            <h1>My Travel Journal</h1>
+        </header>
+    )
+}
---
+++ section2/travel-journal/index.jsx
@@ -0,0 +1,18 @@
+// import { createRoot } from "react-dom/client"
+// import App from 'App.jsx'
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+
+root.render(<App />)
+
+
+/**
+ * Challenge:
+ * 
+ * - Create an App component in a separate file.
+ *   Import it here and render it
+ * - Create a "components" folder and a Header component.
+ *   Render the Header inside the App component.
+ * - Follow the Travel Journal design to build the Header
+ *   for our page.
+ */
